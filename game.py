dd0e81383db5ee88daff6c9cf05494124ae7f978
Mikhail Timofeev <descentcare@gmail.com>
2022-11-07 22:33:42 +0500

    made game.py truecolorutf8 version

--- game.py
+++ game.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 import random
 
+color_spaces = [
+        "\033[48;2;255;0;0m \033[0m",
+        "\033[48;2;0;255;0m \033[0m",
+        "\033[48;2;0;0;255m \033[0m",
+        "\033[48;2;255;255;0m \033[0m",
+        "\033[48;2;255;0;255m \033[0m",
+        "\033[48;2;0;255;255m \033[0m",
+        "\033[48;2;255;255;255m \033[0m",
+        "\033[48;2;120;60;255m \033[0m",
+        ]
+
 fluid_level = 0
 while not (1 < fluid_level < 10):
     fluid_level = int(input("choose fluid level[2-9]: "))
 letters = ''
 while ' ' in letters or len(letters) < 3 or len(letters) > len(set(letters)):
-    letters = input("choose your controll scheme(key for every fluid cup, unique, no spaces, at least 3):")
+    letters = input("Enter your controll scheme:")
 
 class FluidCup():
     def __init__(self, letter=None):
@@ -31,7 +42,7 @@ class FluidCup():
             if len(source_cup) and source_cup.cup[-1] != x:
                 break
     def closed(self):
-        return len(self) == 0 or all(map(lambda c: c == self.cup[0], self.cup))
+        return len(self) == 0 or all(map(lambda c: c == self.cup[0], self.cup)) and len(self) == fluid_level
 
 def init_board(letters):
     fluid = list(letters) * fluid_level
@@ -44,14 +55,24 @@ def init_board(letters):
     return board
 
 board = init_board(letters)
+letter_colors = dict()
+if len(letters) > len(color_spaces):
+    for l in letters:
+        letter_colors[l] = l
+else:
+    for i in range(len(letters)):
+        letter_colors[letters[i]] = color_spaces[i]
 letters += ' '
+letter_colors[' '] = ' '
+
 choices = dict()
 for c in board:
     choices[c.letter] = c
 
+ub='┗━┛'
 def print_board(board):
     print('\033[2J\033[2;H\033[m', end='')
-    print(''.join(f' [{x}] ' for x in letters))
+    print(''.join(f' {{{x}}} ' for x in letters))
     print()
     for i in range(fluid_level-1, -1, -1):
         row = []
@@ -60,8 +81,8 @@ def print_board(board):
                 row.append(c.cup[i])
             else:
                 row.append(' ')
-        print(''.join(f' [{x}] ' for x in row))
-
+        print(''.join(f' ┃{letter_colors[x]}┃ ' for x in row))
+    print(' ┗━┛ ' * len(row)) 
 turns = 0
 while True:
     print_board(board)
